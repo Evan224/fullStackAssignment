55f8e4b7e073bb96a40b390f504460225fd8c865
Evan <2248392717@qq.com>
2022-09-07 20:13:35 +0300

    2.11: The Phonebook Step6

--- part2/ThePhoneBook/src/App.js
+++ part2/ThePhoneBook/src/App.js
@@ -1,20 +1,14 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-
-
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -36,7 +30,6 @@ const App = () => {
   }
 
   const addPerson = (event) => {
-    console.log(event)
     event.preventDefault();
     if(checkName(newName)) {
       alert(`${newName} is already added to phonebook`)
@@ -51,34 +44,21 @@ const App = () => {
     setNewNumber('')
   }
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   return (
     <div>
       <h2>Phonebook</h2>
-      {/* <form>
-        <div>
-          filter shown with: <input value={filter} onChange={handleFilterChange}/>
-        </div>
-      </form> */}
       <Filter filter={filter} handleFilterChange={handleFilterChange}/>
       <h2>add a new</h2>
-      {/* <form>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit" onClick={addName}>add</button>
-        </div>
-      </form> */}
       <PersonForm newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addPerson={addPerson}/>
       <h2>Numbers</h2>
-      {/* {persons.map(person => {
-        if(person.name.includes(filter)){
-          return <p key={person.name}>{person.name} {person.number}</p>
-        }
-      })} */}
       <Persons persons={persons} filter={filter}/>
     </div>
   )
