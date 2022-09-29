3265a044e4cc335d479538f3e71d2334fb924ca7
Evan <2248392717@qq.com>
2022-09-29 10:24:04 +0300

    5.16: Blog list tests, step4

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import NewBlog from './NewBlog'
 import userEvent from '@testing-library/user-event'
 
 test('renders content',async () => {
@@ -31,8 +32,29 @@ test('renders content',async () => {
   await user.click(likeButton)
   await user.click(likeButton)
 
-
-
   expect(mockHandler.mock.calls).toHaveLength(2)
 //   expect(div).not.toHaveTextContent('test url')
+})
+
+test('add blog content',async () => {
+  const newBlog={
+    title:'new test title',
+    author:'new test author',
+    url:'new test url',
+  }
+
+  const mockHandler = jest.fn()
+
+  const { container }=render(<NewBlog addCallback={mockHandler} blog={newBlog}/>)
+
+
+  const user = userEvent.setup()
+  const button = screen.getByText('create')
+
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+
+  expect(mockHandler.mock.calls[0][0].title).toBe('new test title')
+  console.log(mockHandler.mock.calls)
 })
--- part5/bloglist-frontend/src/components/NewBlog.js
+++ part5/bloglist-frontend/src/components/NewBlog.js
@@ -1,59 +1,66 @@
-import { useState } from "react"
+import { useState } from 'react'
 import loginService from '../services/login'
 import blogService from '../services/blogs'
 
-const NewBlog=({addCallback})=>{
-    const [title,setTitle]=useState('')
-    const [author,setAuthor]=useState('')
-    const [url,setUrl]=useState('')
-    
-    const createBlog=async (event)=>{
-        event.preventDefault()
-        const blogObject={
-        title:title,
-        author:author,
-        url:url,
-        }
-        const response=await blogService.create(blogObject)
-        // console.log(response)
-        addCallback(response)
-        setTitle('')
-        setAuthor('')
-        setUrl('')
-    }
+const NewBlog=({ addCallback,blog }) => {
+  const [title,setTitle]=useState('')
+  const [author,setAuthor]=useState('')
+  const [url,setUrl]=useState('')
 
-    return(
-        <form>
-            <div>
+  const createBlog=async (event) => {
+    event.preventDefault()
+    let blogObject={
+      title:title,
+      author:author,
+      url:url,
+    }
+    await console.log(blog)
+    try{
+       const response=await blogService.create(blogObject)
+
+    }catch{
+      console.log('error')
+    }
+    addCallback({title:blog.title})
+    // console.log(response)
+    // addCallback(response)
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  return(
+    <form>
+      <div>
             title
-            <input
-              type="text"
-              value={title}
-              name="Username"
-              onChange={({ target }) => setTitle(target.value)}
-            />
-           </div>
-           <div>
+        <input
+          type="text"
+          value={title}
+          name="Username"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
             author
-            <input
-              type="text"
-              value={author}
-              name="Username"
-              onChange={({ target }) => setAuthor(target.value)}
-            />
-           </div>
-           <div>
+        <input
+          type="text"
+          value={author}
+          name="Username"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
             url
-            <input
-              type="text"
-              value={url}
-              name="Username"
-              onChange={({ target }) => setUrl(target.value)}
-            />
-           </div>
-           <button onClick={createBlog}>create</button>
-        </form>
-    )
+        <input
+          type="text"
+          value={url}
+          name="Username"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button onClick={createBlog}>create</button>
+    </form>
+  )
 }
 
-export default NewBlog;
+export default NewBlog
